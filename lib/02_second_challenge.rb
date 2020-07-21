4abfd02d0a45f19d7cd923abcbfdd5819ce68618
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-21 14:27:40 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,15 +1,15 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []#your key/value pairs here!
       },
     :capulet => {
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []#your key/value pairs here!
       }
     }
